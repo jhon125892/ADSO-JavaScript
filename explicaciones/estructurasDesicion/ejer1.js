5aa3b3af754090e31aefcd8e16e929ddcdd81eba
jhon125892 <johnsteven747474@gmail.com>
2025-08-05 12:25:59 -0500

    ejercicios

--- explicaciones/estructurasDesicion/ejer1.js
+++ explicaciones/estructurasDesicion/ejer1.js
@@ -1,2 +1,12 @@
 //Uso if basico
 
+const prompt = require('prompt-sync')();
+
+let valor = parseInt(prompt("Ingrese un numero: "));
+if (valor >= 10 && valor <= 99) {
+    console.log("Tiene dos dígitos");
+} else {
+    console.log("No tiene dos dígitos");
+}
+
+
