f656cd05fef1ba97e97e41fbe8fe9779efc84231
jhon125892 <johnsteven747474@gmail.com>
2025-08-05 00:14:30 -0500

    ejercicio 1, taller 5-8-25

--- Ejercicio-1.js
+++
@@ -1,4 +0,0 @@
-let x=5
-
-console.log(x);
-console.log("Hola mundo");
---
+++ taller 5-8-25/ejercicio1.js
@@ -0,0 +1,6 @@
+let precio=3000;
+let iva=19;
+
+let total=precio + (precio * iva / 100);
+
+console.log("El total a pagar es de: $" + total);
