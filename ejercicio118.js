fe83603ee79b204a28727d63160d0dec9c04de55
jhon125892 <johnsteven747474@gmail.com>
2025-08-11 12:15:12 -0500

    edit

--- ejercicio118.js
+++ ejercicio118.js
@@ -17,20 +17,33 @@ function menu(){
 
             switch(opcion) {
         case 1:
+
             let radio=0
             radio=parseFloat(prompt("Ingrese el area del circulo: "))
             area= Math.PI * (radio ** 2)
             console.log(`El area de tu circulo es: ${area}`)
             prompt("Presione Enter para continuar...");
             break;
+
         case 2:
-            console.log("2");
-            prompt("Presione Enter para continuar...");
+
+            let numero=0
+            let numero1=0
+            numero= Math.floor(Math.random() * 100) + 1;
+            numero1= parseInt(prompt("Se ha generado un numero aleatorio del 1 hasta el 100, advina uno.. a ver si le atinas: "))
+            if (numero1 === numero) {
+                console.log("Le atinaste! Felicidades")
+            } else {
+                console.log(`No le atinaste, el número era ${numero}. ¡Suerte para la próxima!`)
+            }
             break;
+
         case 3:
+
             console.log("3");
             prompt("Presione Enter para continuar...");
             break;
+
         default:
             console.log("Opción inválida");
             prompt("Presione Enter para continuar...");
