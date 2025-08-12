c4e5df4c8684e33586ec7f2812fb1d75f335fe36
jhon125892 <johnsteven747474@gmail.com>
2025-08-12 10:57:02 -0500

    awawa

--- 1.js
+++ 1.js
@@ -1,8 +1,11 @@
+// node 1.js
+const prompt = require('prompt-sync')();
+
 let cuenta= {
     titular: "Monica Galindo",
-    saldo: "2500000",
+    saldo: 2500000,
     mostrarSaldo: function(){
-        return `La cuenta de ${this.titular} su saldo es ${saldo}`
+        return `La cuenta de ${this.titular} su saldo es ${this.saldo}`
     }
 
 }
---
+++ 2.js
@@ -0,0 +1,7 @@
+// ARREGLOS
+
+let frutas=["Manzana", "Pera", "Kiwi"]
+console.log(frutas[0]) // Manzana
+console.log(frutas[1]) // Pera
+console.log(frutas[2]) // Kiwi
+console.log(frutas.length)
