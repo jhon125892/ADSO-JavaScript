7d4e8e96d010567ec838f86f2905921f245ec038
jhon125892 <johnsteven747474@gmail.com>
2025-08-22 07:25:54 -0500

    edit

--- -- explicaciones/arreglos_explicacion.js
+++ -- explicaciones/arreglos_explicacion.js
@@ -5,3 +5,12 @@ console.log(frutas[0]) // Manzana
 console.log(frutas[1]) // Pera
 console.log(frutas[2]) // Kiwi
 console.log(frutas.length)
+frutas.push("Banano")
+console.log(frutas)
+frutas.pop
+console.log(frutas)
+
+
+//
+
+
